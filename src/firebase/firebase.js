7d2db6ebb31cd94e212adbd67a58f028620d734d
Airi <aiai9004432221@gmail.com>
2025-07-09 23:07:33 +0900

    2

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,37 @@
 // src/firebase/firebase.js
 import { initializeApp } from "firebase/app"
-import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs } from "firebase/firestore"
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  getDocs
+} from "firebase/firestore"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtvi17_M3FUpf_OywZoMMcmZKiU8z_mBc",
   authDomain: "trigger-ad101.firebaseapp.com",
   projectId: "trigger-ad101",
-  storageBucket: "trigger-ad101.firebasestorage.app",
+  storageBucket: "trigger-ad101.appspot.com", // ← typoも修正（storageBucketのドメインおかしかった）
   messagingSenderId: "769144492125",
   appId: "1:769144492125:web:45aca7e6b70a139d823db4",
   measurementId: "G-RW676RF4PH"
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
-const db = getFirestore(app) // ← これが抜けてた！
+const db = getFirestore(app)
 
-export { db, collection, addDoc, query, orderBy, limit, getDocs }
+// ここで全部まとめて export！
+export {
+  db,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  getDocs
+}
